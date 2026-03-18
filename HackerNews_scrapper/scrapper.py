3dc8d435f1b8b520756afb1e2adf0dc92f00e9a5
Sidney <sidney.0503c@gmail.com>
2026-03-18 12:41:45 +0000

    extract relevant info from each article

--- HackerNews_scrapper/scrapper.py
+++ HackerNews_scrapper/scrapper.py
@@ -4,8 +4,7 @@ from requests.exceptions import HTTPError, Timeout, RequestException
 import time
 
 def scrape(num_pages):
-    all_stories = []
-    
+
     for page in range(1, num_pages + 1):
         url = f"https://news.ycombinator.com/?p={page}"
         print(f"--- Scraping Page {page}")
@@ -16,13 +15,47 @@ def scrape(num_pages):
             response.raise_for_status()
             print("Success! Data retrieved.")
             soup = BeautifulSoup(response.text, "lxml")
-            stories = soup.find_all("span", class_ = "titleline")
+            # fill in anything missing with default values
+            articles_list = []
+            rows = soup.find_all("tr", class_="athing submission")
+            
+            for row in rows:
+                article_num = row.find("span", class_="rank").text[:-1]
+                title_line = row.find("span", class_="titleline")
 
-            for story in stories:
-                title = story.get_text()
-                link = story.find("a")["href"]
-                all_stories.append({"title": title, "link": link})
-        
+                title = title_line.find("a").text if title_line else "N/A"
+                url = title_line.find("a")["href"] if title_line else "N/A"
+                website = title_line.find("span", class_="sitestr").text if title_line else "N/A"
+
+                # gets next <tr> after current one
+                subtext_row = row.find_next_sibling("tr")
+
+                score = 0
+                author = "N/A"
+                time_posted = "N/A"
+
+                if subtext_row:
+                    score_tag = subtext_row.find("span", class_="score")
+                    author_tag = subtext_row.find("a", class_="hnuser")
+                    age_tag = subtext_row.find("span", class_="age")
+                    if score_tag:
+                        score = score_tag.text
+                    if author_tag:
+                        author = author_tag.text
+                    if age_tag:
+                        time_posted = age_tag.a.text
+                
+                article = {
+                    "Article Number": article_num,
+                    "Article Title": title,
+                    "Source Website": website,
+                    "Source URL": url,
+                    "Article Author": author,
+                    "Article Score": score,
+                    "Posted": time_posted
+                }
+                articles_list.append(article)
+
         except Timeout:
             print("Error: The request timed out. Check your internet of the server status.")
         
@@ -37,7 +70,8 @@ def scrape(num_pages):
         # to prevent hitting the server too fast
         time.sleep(1)
 
-    print(f"Gathered {len(all_stories)} stories")
+    print(f"Gathered {len(articles_list)} articles")
+    print(articles_list)
     return
 
 if __name__ == "__main__":
